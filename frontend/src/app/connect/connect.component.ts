e6a4f8f8c4401bc8ebb959cb04cbfcb16e9596a3
novucs <contact@novucs.net>
2017-07-07 13:35:21 +0100

    Design the connect page to provide the user with options

--- frontend/src/app/connect/connect.component.ts
+++ frontend/src/app/connect/connect.component.ts
@@ -9,15 +9,23 @@ import { AttendanceApiService } from '../service/attendance-api.service';
 })
 export class ConnectComponent implements OnInit {
 
+  currentSchedules: any = [];
   schedules: any = [];
+  students: any = [];
 
-  constructor(private attendanceApiService: AttendanceApiService) {
+  constructor(private api: AttendanceApiService) {
   }
 
   ngOnInit() {
     // Retrieve posts from the API
-    this.attendanceApiService.getCurrentSchedules().subscribe(s => {
+    this.api.getCurrentSchedules().subscribe(s => {
+      this.currentSchedules = s.data;
+    });
+    this.api.getSchedules().subscribe(s => {
       this.schedules = s.data;
+    });
+    this.api.getStudents().subscribe(s => {
+      this.students = s.data;
     });
   }
 }
