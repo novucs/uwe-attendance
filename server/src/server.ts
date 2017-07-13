bb2390ca92a406560c8861ecffd0bddb9ce022c3
novucs <contact@novucs.net>
2017-07-13 14:04:46 +0100

    Send tag data to clients

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 import * as WebSocket from "ws";
 import * as mongoose from "mongoose";
 import {attendanceRouter, scheduleRouter, studentRouter} from "./controllers";
-import * as nfc from "nfc";
+import {nfc} from "nfc";
 import * as util from "util";
 
 const app: Application = express();
@@ -18,16 +18,16 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(express.static(path.join(__dirname, '../../public/build')));
+app.use(express.static(path.join(__dirname, "../../public/build")));
 
-app.use('/api', scheduleRouter);
-app.use('/api', studentRouter);
-app.use('/api', attendanceRouter);
+app.use("/api", scheduleRouter);
+app.use("/api", studentRouter);
+app.use("/api", attendanceRouter);
 
 // Serve up the Angular app.
 app.get('*', (req, res) => {
     console.log("Serving client");
-    res.sendFile(path.join(__dirname, '../../public/build/index.html'));
+    res.sendFile(path.join(__dirname, "../../public/build/index.html"));
 });
 
 // Serve the application at the given port.
@@ -38,18 +38,30 @@ app.listen(port, () => {
 // Setup the NFC reader via WebSocket.
 const portNFC = process.env.PORT || 3001;
 const server = new WebSocket.Server({port: portNFC});
+const clients = [];
 
-server.on('connection', ws => {
+server.on("connection", ws => {
     console.log("WebSocket connection made");
+
+    ws.on("close", ws => {
+        const index = clients.indexOf(ws, 0);
+
+        if (index > -1) {
+            clients.splice(index, 1);
+        }
+    });
     // ws.send("123456");
 });
 
-console.log("nfc.version(): " + util.inspect(nfc.nfc.version(), {depth: null}));
-console.log("nfc.scan(): " + util.inspect(nfc.nfc.scan(), {depth: null}));
-const device = new nfc.nfc.NFC();
+console.log("nfc.version(): " + util.inspect(nfc.version(), {depth: null}));
+console.log("nfc.scan(): " + util.inspect(nfc.scan(), {depth: null}));
+const device = new nfc.NFC();
+
 device.on("read", tag => {
     if ((!!tag.data) && (!!tag.offset)) {
-        console.log(util.inspect(nfc.nfc.parse(tag.data.slice(tag.offset)), {depth: null}));
+        const tagData = util.inspect(nfc.parse(tag.data.slice(tag.offset)));
+        console.log(tagData, {depth: null});
+        clients.forEach(client => client.send(tagData));
     }
 }).on("error", error => {
     console.log("An error occurred while reading the NFC device: ", error);
