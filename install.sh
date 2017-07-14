b9d5f48f298dba516a205b44d34bdbfd8fdf03df
novucs <contact@novucs.net>
2017-07-14 12:58:46 +0100

    Ensure database importing ends and create installation script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Prerequisites
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install nodejs npm mongodb
+sudo npm install -g @angular/cli
+sudo systemctl start mongodb
+sudo apt-get install libusb-dev dh-autoreconf
+
+# LibNFC
+git clone https://github.com/nfc-tools/libnfc.git
+cd libnfc
+git checkout libnfc-1.7.1
+git clean -d -f -x
+git remote|grep -q anonscm||git remote add anonscm git://anonscm.debian.org/collab-maint/libnfc.git
+git fetch anonscm
+git checkout remotes/anonscm/master debian
+git reset
+dpkg-buildpackage -uc -us -b
+sudo apt-get install libusb-0.1-4
+sudo dpkg -i ../libnfc*.deb
+
+# Public
+cd ../public
+npm install
+ng build
+
+# Scripts
+cd ../scripts
+npm install
+tsc
+npm start
+
+# Server
+cd ../server
+npm install
+tsc
+npm start
--- scripts/src/run.ts
+++ scripts/src/run.ts
@@ -1,9 +1,19 @@
 import {readFile} from "fs";
 import {connect, model, Schema} from "mongoose";
 
-connect("mongodb://localhost/attendance", {
-    useMongoClient: true
-});
+interface Student {
+    _id: Schema.Types.ObjectId;
+    tag: string;
+    name: string;
+    groups: string[];
+}
+
+export interface Session {
+    _id: Schema.Types.ObjectId;
+    event: string;
+    onDate: Date;
+    groups: string[];
+}
 
 const studentSchema = new Schema({
     tag: String,
@@ -17,63 +27,105 @@ const scheduleSchema = new Schema({
     groups: [String]
 }, {collection: "schedule"});
 
-readFile("students.csv", "utf8", (error, data) => {
-    if (error) {
-        throw error;
-    }
+function updateStudent(name: string, groups: string[]): Promise<Student> {
+    const query = {"name": name};
+    const update = {"tag": '', "groups": groups};
+    const options = {upsert: true};
 
-    const lines = data.toString().split('\n');
-
-    lines.forEach((line) => {
-        if (!line || line.substring(0, 1) == '#') {
-            return;
-        }
-
-        const studentData = line.split(',');
-        const name = studentData[0];
-        const groups = studentData.slice(1, studentData.length);
-
-        const query = {"name": name};
-        const update = {"tag": '', "groups": groups};
-        const options = {upsert: true};
-
+    return new Promise<Student>((fulfill, reject) => {
         model("students", studentSchema).findOneAndUpdate(query, update, options, (error, student) => {
             if (error) {
-                throw error;
+                reject(error);
+                return;
             }
 
             console.log("Created student: " + name);
+            fulfill(student);
         });
     });
-});
+}
 
-readFile("schedule.csv", "utf8", (error, data) => {
-    if (error) {
-        throw error;
-    }
+function updateStudents(): Promise<[Student]> {
+    return new Promise<[Student]>((fulfill, reject) => {
+        readFile("students.csv", "utf8", (error, data) => {
+            if (error) {
+                throw reject(error);
+            }
 
-    const lines = data.toString().split("\n");
+            const lines = data.toString().split('\n');
+            const promises = [];
 
-    lines.forEach((line) => {
-        if (!line || line.substring(0, 1) == '#') {
-            return;
-        }
+            lines.forEach((line) => {
+                if (!line || line.substring(0, 1) == '#') {
+                    return;
+                }
 
-        const sessionData = line.split(',');
-        const event = sessionData[0];
-        const onDate = new Date(sessionData[1]);
-        const groups = sessionData.slice(2, sessionData.length);
+                const studentData = line.split(',');
+                const name = studentData[0];
+                const groups = studentData.slice(1, studentData.length);
+                promises.push(updateStudent(name, groups));
+            });
 
-        const query = {"event": event};
-        const update = {"onDate": onDate, "groups": groups};
-        const options = {upsert: true};
+            fulfill(Promise.all(promises));
+        });
+    });
+}
 
+function updateSession(event: string, onDate: Date, groups: string[]): Promise<Session> {
+    const query = {"event": event};
+    const update = {"onDate": onDate, "groups": groups};
+    const options = {upsert: true};
+
+    return new Promise<Session>((fulfill, reject) => {
         model("schedule", scheduleSchema).findOneAndUpdate(query, update, options, (error, session) => {
             if (error) {
-                throw error;
+                reject(error);
+                return;
             }
 
             console.log("Created schedule: " + event + " on " + onDate);
+            fulfill(session);
         });
     });
+}
+
+function updateSchedule(): Promise<[Session]> {
+    return new Promise<[Session]>((fulfill, reject) => {
+        readFile("schedule.csv", "utf8", (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            const lines = data.toString().split("\n");
+            const promises = [];
+
+            lines.forEach((line) => {
+                if (!line || line.substring(0, 1) == '#') {
+                    return;
+                }
+
+                const sessionData = line.split(',');
+                const event = sessionData[0];
+                const onDate = new Date(sessionData[1]);
+                const groups = sessionData.slice(2, sessionData.length);
+
+                promises.push(updateSession(event, onDate, groups));
+            });
+
+            fulfill(Promise.all(promises));
+        });
+    });
+}
+
+connect("mongodb://localhost/attendance", {
+    useMongoClient: true
+});
+
+const studentPromise = updateStudents();
+const schedulePromise = updateSchedule();
+
+Promise.all([studentPromise, schedulePromise]).then(([students, schedules]) => {
+    console.log("Successfully updated " + students.length + " students, and " + schedules.length + " schedules");
+    process.exit();
 });
